453b5c280bba82fc39d161eaf074966edd9bad9a
achaldhote02 <achaldhote02@gmail.com>
2020-04-23 02:02:34 +0530

    add two component property and class

--- src/app/class/class.component.ts
+++ src/app/class/class.component.ts
@@ -2,14 +2,47 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-class',
-  templateUrl: './class.component.html',
-  styleUrls: ['./class.component.css']
+  template: `
+  <h2>Welcome11 {{name}}</h2>
+  <h2 class="text-green">Soni<h2>
+  <h2 [class]="succesClass">Bhawna<h2>
+  <h2 class="text-italic" [class]="succesClass"> Diksha </h2>
+  <h2 [class.text-red]="hasError">Dushyal</h2>
+  `,
+  styles: [`
+  .text-green {
+    color:green;
+    font-size:20px;
+  }
+  .text-red {
+    color:red;
+    font-size:20px;
+
+  }
+  .text-italic {
+font-style:italic;
+font-size:20px;
+
+  }
+
+  `]
 })
 export class ClassComponent implements OnInit {
 
-  constructor() { }
+  public name = "vinoda";
+  public succesClass = "text-succes";
+  public hasError = false;
+  public isSpecial = true;
+  public massageClasses = {
+    "text- green":!this.hasError,
+      "text-red" : this.hasError,
+        "text-italic": this.isSpecial
 
-  ngOnInit(): void {
   }
+
+constructor() { }
+
+ngOnInit(): void {
+}
 
 }
--- src/app/property/property.component.ts
+++ src/app/property/property.component.ts
@@ -6,12 +6,22 @@ import { Component, OnInit } from '@angular/core';
 <h2>
 welcome {{name}}
 </h2>
+<input type="text" value="Ganesh">
+<input [id]="myId" type="text" value="Ganesh">
+<input id="{{myId}}" type="text" value="Ganesh">
+<input disabled id="{{myId}}" type="text" value="Ganesh">
+<input [disabled]="false" id="{{myId}}" type="text" value="Ganesh">
+<input [disabled]="isDisabled" id="{{myId}}" type="text" value="Ganesh">
+<input bind-disabled="isDisabled" id="{{myId}}" type="text" value="Ganesh">
+
   `,
   styleUrls: ['./property.component.css']
 })
 export class PropertyComponent implements OnInit {
 
   public name = "Achal1";
+  public myId = "userId";
+  public isDisabled=false;
   constructor() { }
 
   ngOnInit(): void {
